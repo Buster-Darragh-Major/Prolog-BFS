f2cf620be38488ee24567a4d8dbb0fd04cbf7093
Buster Darragh-Major <busterjdarraghmajor@gmail.com>
2018-09-20 12:12:52 +1200

    REmocve facts and FOL

--- family.pl
+++ family.pl
@@ -2,31 +2,17 @@
 % DEFINITIONS:
 
 % motherOf
-% MotherOf(x, y) = ChildOf(y, x) and Female(x)
 motherOf(Mother, Person) :- childOf(Person, Mother), female(Mother).
 
 % sisterOf
-% SisterOf(x, y) = E(p)ChildOf(x, p) and ChildOf(y, p) and Female(x) and x not y
+% 
 sisterOf(Sister, Person) :- childOf(Sister, P1), 
 			childOf(Person, P1), 
 			female(Sister), 
 			not(Sister = Person).
-
-	
-parentOf(a, b).
-parentOf(b, c).
-parentOf(c, d).
-parentOf(c, e).
-parentOf(c, f).
-parentOf(f, g).
-parentOf(g, h).
-
-parentOf(x, y).
-parentOf(y, z).
-parentOf(x, x1).
-parentOf(x, y2).
 	
 
+% inSameTree 
 inSameTree(Node1, Node2) :-
 	getRootNode(Node1, Root1),
 	getRootNode(Node2, Root2),
